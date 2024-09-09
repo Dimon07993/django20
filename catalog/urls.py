838f06b3a3b11ffd7b2ad8fafadc52ce29d61e66
qw <qw>
2024-09-10 00:11:13 +0300

    фвв

---
+++ catalog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import index_list, product_detail
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', index_list, name='product_list'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
+
+]
